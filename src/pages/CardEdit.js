393251358e33a678f516b8bac8a1e6f0ed5d284d
Vivien Guo <guov@student.unimelb.edu.au>
2022-09-10 15:21:13 +1000

    card nav styling + blank cardedit page

---
+++ src/pages/CardEdit.js
@@ -0,0 +1,13 @@
+import "../styles/CardEdit.scss"
+import React from "react";
+import { useParams } from "react-router-dom";
+
+const CardEdit = () => {
+  const cardId = useParams().id;
+
+  return (
+    <></>
+  )
+}
+
+export default CardEdit;
