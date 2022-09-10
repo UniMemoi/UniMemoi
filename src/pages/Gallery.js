2754a99f2cff4e91b9ebca02b7dee2de531e78e6
avlanaz <87753607+avlanaz@users.noreply.github.com>
2022-09-11 08:11:12 +1000

    Add template for CardEdit

--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -2,7 +2,6 @@ import { Card, Filter } from "../components";
 import "../styles/Gallery.scss"
 import { UploadPopup } from "../components";
 import { Col, InputGroup, Row } from 'reactstrap';
-import { FaSearch } from 'react-icons/fa';
 import React from "react";
 
 
@@ -28,23 +27,18 @@ const sortOpts = {
   "Name": sortName
 }
 
-const filterSubj = (v) => {
-  console.log(`filter by subj ${v}`);
+const filterSubj = () => {
+  console.log("filter by subj");
 }
 
-const filterDateFrom = (v) => {
-  console.log(`filter by date from ${v}`);
+const filterDate = () => {
+  console.log("filter by date");
 }
 
-const filterDateTo = (v) => {
-  console.log(`filter by date to ${v}`);
+const filtOpts = {
+  "Date": filterDate,
+  "Subject": filterSubj
 }
-
-const filtOpts = [
-  ["subject", filterSubj, "text"],
-  ["date from", filterDateFrom, "date"],
-  ["date to", filterDateTo, "date"],
-]
 
 const Gallery = () => {
   const [isOpen, setIsOpen] = React.useState(false);
@@ -59,8 +53,8 @@ const Gallery = () => {
   }
 
   const popupContent = 
-  <div className="popup-content">
-      <form action="/upload" method="POST" encType="multipart/form-data">
+  <div>
+      <form action="/upload" method="POST" encType="multipart/form-data"  className="popup-content">
         <div>
           <label for="avatar">Upload a slide (.ppt) file: </label>
         
@@ -74,8 +68,13 @@ const Gallery = () => {
             type="submit"
             value="Submit"
             className={"text-button"}
-            onClick="location.href='https://google.com'"
+            onClick="changePage"
         > Confirm </button>
+        <script>
+          function changePage() {
+              // windows.location.href = link("https://google.com");
+          }
+        </script>
       </form>
   </div>;
 
@@ -97,6 +96,7 @@ const Gallery = () => {
 
         <InputGroup>
           <input />
+            
           <button className={"text-button"}>Search</button>
         </InputGroup>
 
@@ -111,19 +111,11 @@ const Gallery = () => {
             })}
           </Filter>
           <Filter action="Filter">
-            {filtOpts.map((item) => {
-              return <InputGroup className={"ndropdown-item nav-filt"} key={item[0]}>
-                <input
-                  type="text" id={`${item[0]}-input`} 
-                  onFocusCapture={e => e.target.type=item[2]}
-                  onBlurCapture={e => e.target.type="text"}
-                  placeholder={`Enter ${item[0]}...`} />
-                <button onClick={() => {
-                  item[1](document.getElementById(`${item[0]}-input`).value)
-                }}><FaSearch /></button>
-              </InputGroup>
+            {Object.entries(filtOpts).map((item) => {
+              return <button className={"ndropdown-item"} onClick={item[1]}>
+                {item[0]}
+              </button>
             })}
-            <button className={"ndropdown-item"}>Clear</button>
           </Filter>
         </div>
         <div className={"card-grid"}>
