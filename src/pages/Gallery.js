012a47f4a8bea0620f3a1f64c91a8f0d3fd93619
Vivien Guo <guov@student.unimelb.edu.au>
2022-09-10 13:54:42 +1000

    format

--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -1,6 +1,6 @@
 import { Card } from "../components";
 import "../styles/Gallery.scss"
-import { Col, Row } from 'reactstrap';
+import { Button, Col, Input, InputGroup, Row } from 'reactstrap';
 
 
 const tempCard = {
@@ -14,19 +14,26 @@ const tempCardList = [
 
 const Gallery = () => {
   return (
-    <section className="gallery">
-      <div className="topics">
-        <button className={"text-button"}>+ Add Topic</button>
-      </div>
-      <div className={"card-grid"}>
-        <Row md="4">
-          {tempCardList.map((c, i) =>
-            <Col key={i}><Card front={c.front} back={c.back}/></Col>
-          )}
-        </Row>
-      </div>
-      <div>flip page</div>
-    </section>
+    <div className="gallery">
+    <div className="topics">
+      <button className={"text-button"}>+ Add Topic</button>
+      <InputGroup>
+        <input />
+        <button className={"text-button"}>Search</button>
+      </InputGroup>
+    </div>
+      <section>
+        <div className={"card-nav"}>something</div>
+        <div className={"card-grid"}>
+          <Row md="4">
+            {tempCardList.map((c, i) =>
+              <Col key={i}><Card front={c.front} back={c.back}/></Col>
+            )}
+          </Row>
+        </div>
+        <div>flip page</div>
+      </section>
+    </div>
   )
 }
 
