614aab77d745accc5124468b61ec4b03e6bf5baf
Vivien Guo <guov@student.unimelb.edu.au>
2022-09-11 10:04:10 +1000

    changes

--- src/App.js
+++ src/App.js
@@ -2,18 +2,22 @@ import "./styles/Global.scss";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Home, Gallery, CardEdit } from "./pages";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import logo from "./images/temp_logo.png";
 
 function App() {
   return (
     <div className="App">
       <header>
-        <img src="../public/temp_logo.png" alt="UniMemoi logo" />
-        text
+        <img src={logo} alt="unimemoi logo" />
+        <span>UniMemoi</span>
+        <div>&nbsp;</div>
       </header>
       <main>
         <nav>
-          <a href="/">Home</a>
-          <a href="gallery">Gallery</a>
+          <div>
+            <a href="/">Home</a>
+            <a href="gallery">Gallery</a>
+          </div>
         </nav>
         <div className="content">
           <Router>
